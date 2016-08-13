0ae874cd302ee952fd0ec71189e112f9e1583355
wouzar <wouzar@gmail.com>
2016-08-13 12:12:15 +0300

    Add OpenId logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,22 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
 
+# App initialization
 app = Flask(__name__)
 app.config.from_object('config')
+
+# DB initialization
 db = SQLAlchemy(app)
 
+# Login initialization
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views, models
+
